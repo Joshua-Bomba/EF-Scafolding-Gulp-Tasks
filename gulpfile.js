c51fdd0e4bcb866aecc6f4c507b7548e4a86c198
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-04-16 15:13:41 -0400

    Add project files.

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+﻿//Packages
+const del = require('del');
+//var gulp = require('gulp');
+var path = require('path');
+let PowerShell = require('node-powershell');
+
+let settings = require('./settings.json');
+
+var ProjectFolder = path.resolve(__dirname, settings.EFProjectFolder);
+
+var _contextProvider = settings.Provider;
+var _contextClass = settings.ContextClassName;
+var _namespace = settings.OutputNamespace;
+var outputDir = settings.OutputDirectory;
+
+
+function getConnectionString() {
+    if (typeof settings.ConnectionStrings.value !== 'undefined' && settings.ConnectionStrings.value !== null) {
+        return settings.ConnectionStrings.value;
+    }
+    else if (typeof settings.ConnectionStrings.refFile !== 'undefined') {
+        let appSettings = require(path.join(ProjectFolder, settings.ConnectionStrings.refFile.fileLocation));
+        let access = settings.ConnectionStrings.refFile.access;
+        let con = appSettings;
+        for (let i = 0; i < access.length; i++) {
+            con = con[access[i]];
+        }
+        return con;
+    }
+    else {
+        throw "No Connection String Found";
+    }
+}
+
+async function buildDbContext() {
+    await ScaffoldDbContext();
+}
+
+async function cleanModelsFolder() {
+    await del(path.join(ProjectFolder, `${outputDir}/*`), {force: true});
+}
+
+async function installScafoldDependencies() {
+    let ps = new PowerShell.PowerShell({ debugMsg: false, spawnOptions: { cwd: ProjectFolder } });
+    try {
+
+        ps.streams.stdout.pipe(process.stdout);
+        let pkgsToInstall = settings.Dependencies;
+        for (let i = 0; i < pkgsToInstall.length; i++) {
+            try {
+                console.log(`running ${pkgsToInstall[i]}`);
+                await ps.invoke(pkgsToInstall[i]);
+            }
+            catch {
+                console.log("Exception throw, continuing ");
+            }
+        }
+    }
+    finally {
+        ps.dispose();
+    }
+}
+
+
+exports.buildDbContext = buildDbContext;
+exports.installScafoldDependencies = installScafoldDependencies;
+exports.cleanModelsFolder = cleanModelsFolder;
+
+
+async function ScaffoldDbContext() {
+    let ps = new PowerShell.PowerShell({ debugMsg: false, spawnOptions: { cwd: ProjectFolder } });
+
+    try {
+        let conString = getConnectionString();
+        let str = `dotnet ef dbcontext scaffold "${conString}"  ${_contextProvider} -o "${outputDir}" -c "${_contextClass}" --context-namespace ${_namespace} -d --no-onconfiguring`;
+        console.log(str);
+        ps.streams.stdout.pipe(process.stdout);
+        await ps.invoke(str);
+    }
+    finally {
+        ps.dispose();
+    }
+}
---
+++ vs-ts-fix.js
@@ -0,0 +1,75 @@
+//in package.json
+//{
+// ...
+// "scripts":
+// {
+//   "postinstall": "node build/vs-tsconfig-fix.js"
+// }
+// ...
+//}
+
+//packages have tsconfig.json commited which visual studio is looking for
+//visual studio will find these and get very confused
+//we are going to remove them
+var path = require('path');
+const fs = require('fs');
+
+let rootDir = path.resolve(__dirname);
+
+//let's use the package-lock.json or npm-shrinkwrap.json to find the location of all the modules
+let lock = null;
+if(fs.existsSync(path.join(rootDir,'package-lock.json')))
+{
+    lock = require('../package-lock.json');
+}
+else if(fs.existsSync(path.join(rootDir,'npm-shrinkwrap.json')))
+{
+    lock = require('../npm-shrinkwrap.json');
+}
+
+console.log('starting tsconfig.json cleanup');
+
+//tsconfig.json are not always in the root
+//I did have this using rimraf before but it was a bit slow
+let additionaLocationsToLook = ['src','dist/lib'];
+
+
+if(lock != null)
+{
+    if(typeof lock.packages !== 'undefined')
+    {
+        for(let packageKey in lock.packages)
+        {
+            if(packageKey.startsWith('node_modules')){
+                let p = path.join(rootDir,packageKey,'tsconfig.json');
+                if(fs.existsSync(p))
+                {
+                    console.log(`Deleted ${p}`);
+                    fs.rmSync(p);
+                }
+                else{
+                    //it checks those folders listed above for a tsconfig.json file
+                    for(let i = 0;i < additionaLocationsToLook.length;i++)
+                    {
+                        p = path.join(rootDir,packageKey,additionaLocationsToLook[i],'tsconfig.json');
+                        if(fs.existsSync(p))
+                        {
+                            console.log(`Deleted ${p}`);
+                            fs.rmSync(p);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else{
+        console.log('no packages found');
+    }
+}
+else{
+    console.log('No lock file found');
+}
+
+
+console.log('ending tsconfig.json cleanup');
